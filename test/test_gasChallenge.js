076597fef6fefc39f988d0e63168cf36905aafd1
Wong Zhi Hang <wong_zhihang@u.nus.edu>
2023-07-22 20:53:37 +0800

    Add files via upload

---
+++ test/test_gasChallenge.js
@@ -0,0 +1,35 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+describe("Deploy Gas Challenge Contract", () => {
+  let gas_contract;
+
+  beforeEach(async () => {
+    const gas_challenge_contract = await ethers.getContractFactory(
+      "gasChallenge"
+    );
+    gas_contract = await gas_challenge_contract.deploy();
+  });
+
+  describe("Compute Gas", () => {
+    it("Should return lower gas", async () => {
+      await gas_contract.notOptimizedFunction();
+      await gas_contract.optimizedFunction();
+    });
+  });
+
+  describe("Check Sum Of Array", () => {
+    it("Should return 0", async () => {
+      // Set initial values in the array
+      await gas_contract.notOptimizedFunction();
+
+      // Set the sum of the array to 0
+      await gas_contract.optimizedFunction();
+
+      // Get sum of the array
+      const sumOfArray = await gas_contract.getSumOfArray();
+
+      expect(sumOfArray).to.equal(0);
+    });
+  });
+});
